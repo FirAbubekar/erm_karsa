ace36495f0b6be634cb68267d1940565f3d88ae1
agungdewantr <agungdewan709@gmail.com>
2026-07-29 14:53:07 +0700

    refactor: consolidate duplicate Bangsal model definitions into a single class structure

--- app/Models/Bangsal.php
+++ app/Models/Bangsal.php
@@ -14,17 +14,3 @@ class Bangsal extends Model
 
     protected $fillable = [];
 }
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Bangsal extends Model
-{
-    protected $table = 'bangsal';
-    protected $primaryKey = 'kd_bangsal';
-    public $incrementing = false;
-    protected $keyType = 'string';
-    public $timestamps = false;
-}
